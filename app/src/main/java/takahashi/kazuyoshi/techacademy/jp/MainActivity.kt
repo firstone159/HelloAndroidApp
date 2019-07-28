6095fb4ee5c0d10a39c658c44e25cd329ed4087b
firstone159 <junk.story8@gmail.com>
2019-07-28 16:55:05 +0900

    最初のコミット

---
+++ app/src/main/java/takahashi/kazuyoshi/techacademy/jp/MainActivity.kt
@@ -0,0 +1,12 @@
+package takahashi.kazuyoshi.techacademy.jp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
